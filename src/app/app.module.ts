cc5ce129354466a5d8d762153392ad66d6578837
shreyask12 <shreyashkotian@gmail.com>
2020-02-10 21:44:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { SalaryFormComponent } from './salary-form/salary-form.component';
+// import bootstrap from 'bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/salary-form/salary-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalaryComponent } from './salary/salary.component';
+
+@NgModule({
+  declarations: [SalaryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SalaryFormModule { }
